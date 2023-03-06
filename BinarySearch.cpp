e42bb889838b5d1bd085a6f09b89c23a312ee76e
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-03-06 11:16:15 +0530

    Create BinarySearch.cpp

---
+++ BinarySearch.cpp
@@ -0,0 +1,19 @@
+// binary search return index of searched value in array..
+int binarysearch(vector<int>&array, int val){
+  int start=0;
+  int end=array.size()-1;
+  while(start<=end){
+    int mid=(start+end)/2;
+    if(array[mid]==val){
+    return mid;
+    }
+    else if{
+      if(array[mid]>val){
+        end=mid-1;
+      }
+      else{
+        start=mid+1;
+      }
+    }
+  return -1;
+}
