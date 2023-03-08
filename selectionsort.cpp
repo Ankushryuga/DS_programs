bd65213f495e76b36a4c7a680b823c4999874830
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-03-08 15:22:12 +0530

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,17 +1,37 @@
-// selection sort is all about selecting smallest value from array and swap with 1st index of array.
-void selectionsort(vector<int>&array){
-  int len=array.size()-1;
+#include<iostream>
+#include<vector>
+
+template<class T>
+void selectionsort(std::vector<T>&a, int len){
   for(int i=0;i<=len;i++){
-    int min=array[i];
+    T min=a[0];
     int minindex=i;
-    for(int j=i;j<=len;j++){
-      if(min>array[j]){
-        min=array[j];
+    for(int j=0;j<=len;j++){
+      if(min>a[j]){
+        min=a[j];
         minindex=j;
       }
     }
-    int temp=array[i];
-    array[i]=array[minindex];
-    array[minindex]=temp;
+    //swap the elements...
+    T temp=a[i];
+    a[i]=a[minindex];
+    a[minindex]=temp;
   }
+}
+
+int main(){
+  std::vector<int>v1={5,45,24,13,11,145,50,3,1,0};
+  int l1=v1.size()-1;
+  selectionsort(v1, l1);
+  for(auto it:v1){
+    std::cout<<it<<" ";
+  }
+  std::cout<<"\n";
+  std::vector<char>v2={'z','f','h','p','o','a'};
+  int l2=v2.size()-1;
+  selectionsort(v2, l2);
+  for(auto it:v2){
+    std::cout<<it<<" ";
+  }
+  return 0;
 }
