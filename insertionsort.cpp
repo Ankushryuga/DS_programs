2c668eed07a37caeb804b384f1b2628aa9c97795
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-03-13 12:52:02 +0530

    Update insertionsort.cpp

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,1 +1,20 @@
 
+// insertion sort, one of the sorting technique.
+#include<iostream>
+#include<vector>
+template<class T>
+std::insertionsort(std::vector<T>& input, int size){
+}
+
+//main method..
+int main(){
+  std::vector<int> v1={42,53,134,1,13,04,89,79};
+  int l1=v1.size()-1;
+  selectionsort(v1,l1);
+  for(auto it: v1){
+    std::cout<<it<<" ";
+  }
+  std::cout<<"\n";
+  
+  return 0;
+}
