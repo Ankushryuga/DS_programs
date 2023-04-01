4271b99ee755d875992c49fe3269aa4a3282e705
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-04-01 17:37:43 +0530

    Update doublylinkedlist.cpp

--- doublylinkedlist.cpp
+++ doublylinkedlist.cpp
@@ -1,57 +1,174 @@
 #include<iostream>
 class Node{
-public:
-  int data;
-  Node* next;
-  Node* prev;
-  Node(){
-  data=0;
-    next=NULL;
-    prev=NULL;
-  }
-  Node(int data){
-    this->data=data;
-    this->next=NULL;
-    this->prev=NULL;
-  }
+	public:
+	int data;
+	Node* next;
+	Node* prev;
+	Node(){
+		data=0;
+		next=NULL;
+		prev=NULL;
+	}
+	Node(int data){
+		this->data=data;
+		this->next=NULL;
+		this->prev=NULL;
+	}
 };
 class doublylinklist{
-Node* head;
-Node* tail;
-  int size=0;
-  public:
-  doublylinklist(){
-  head=tail=NULL;
-  }
-  void insertfront(int);
-  void insertback(int);
-  void display();
-  bool is_empty();
-  int lengthlist();
-  int deletefront();
-  int deleteback();
-  int search(int);
-  void totalNodes();
+	Node* head;
+	Node* tail;
+	int size=0;
+	public:
+	doublylinklist(){
+		head=tail=NULL;
+		size=0;
+	}
+	void insertfront(int); //done
+	void insertback(int); //done
+//	void insertafter(int,int);
+	int deletefront(); // done
+	int deleteback(); // done
+	//int deletespecified(int);
+	void search(int); //done
+	void display(); //done
+	int length(); // done
+	bool is_empty(); // done
 };
-
-
-// insert at front in the doubly linked list::
-void doublylinklist::insertfront(int value){
-  Node* newnode=new Node(value);
-  if(head==NULL){
-    head=tail=newnode;
-  }
-  else{
-    head->prev=newnode;
-    newnode->next=head;
-    head=newnode;
-    size++;
-  }
+// insert at begin of dll.
+void doublylinklist::insertfront(int data){
+Node* newnode=new Node(data);
+if(head==NULL){
+	head=tail=newnode;
+//	return;
+}
+else{
+	head->prev=newnode;
+	newnode->next=head;
+	head=newnode;
+}
+size++;
+}
+// insert at the end..
+void doublylinklist::insertback(int data){
+	Node* newnode=new Node(data);
+	if(tail==NULL){
+		head=tail=newnode;
+	//	return;
+	}
+	else{
+		tail->next=newnode;
+		newnode->prev=tail;
+		tail=newnode;
+	}
+	size++;
+}
+// insert after specified node..
+/*
+void doublylinklist::insertafter(int element, int newelement){
+	Node* newnode=new Node(newelement);
+	Node* temp=head;
+	while(temp->data!=element){
+		temp=temp->next;
+	}
+}
+*/
+//delete front..
+int doublylinklist::deletefront(){
+	if(!is_empty()){
+	if(head==tail){
+		tail=NULL;
+	//	return ;
+	}
+	Node* temp=head;
+	int delelement=temp->data;
+	head=head->next;
+	delete temp;
+	size--;
+	return delelement;
+	}
+	return 0;
+} 
+// delete back.
+int doublylinklist::deleteback(){
+	if(!is_empty()){
+	if(tail==tail){
+		head=NULL;
+	//	return;
+	}
+	Node* temp=tail;
+	int delelement=temp->data;
+	tail=tail->prev;
+	tail->next=NULL;
+	delete temp;
+	size--;
+	return delelement;
+	}
+	return 0;
 }
 
-// insert at the end of dounly linked list..
-void doublylinklist::insertback(int value){
-  Node* newnode=new Node(value);
-
+// search an element 
+void doublylinklist::search(int value){
+	Node* temp=head;
+	while(temp!=NULL){
+	  if(temp->data==value){
+	    std::cout<<"Element is available: "<<std::endl;
+	  }
+	  temp=temp->next;
+	}
 }
+// check list is not empty..
+bool doublylinklist::is_empty(){
+	if(size<=0){
+		return true;
+	}
+	return false;
+}
+// total nodes..
+int doublylinklist::length(){
+	return size;
+}
+
+
+void doublylinklist::display(){
+	Node* temp=head;
+	while(temp!=NULL){
+		std::cout<<temp->data<<" ";
+		temp=temp->next;
+	}
+}
+int main(){
+	doublylinklist *ob=new doublylinklist();
+	ob->insertfront(24);
+	ob->insertback(42);
+	ob->display();
+	ob->search(42);
+	std::cout<<"Total Nodes: "<<ob->length();
+	std::cout<<"Is Empty()"<<ob->is_empty();
+	
+	ob->deletefront();
+	ob->deleteback();
+	ob->search(42);
+	std::cout<<"Total Nodes: "<<ob->length();
+	std::cout<<"Is Empty()"<<ob->is_empty();
+	ob->display();
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
