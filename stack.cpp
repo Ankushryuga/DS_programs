b1086207b618f47b2ace5e21c696e4020fc64fb4
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-04-12 11:08:29 +0530

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,72 @@
+/* stack implementation using linked list in c++ */
+#include<iostream>
+class Node{
+  public:
+  int data;
+  Node* next;
+  Node(){
+  data=0;
+  next=NULL;
+  }
+  Node(int data){
+  this->data=data;
+  this->next=NULL;
+  }
+};
+class stack{
+Node* top;
+public:
+stack(){
+top=NULL;
+}
+void push(int);
+  int pop();
+  void display();
+};
+
+void stack::push(int data){
+Node* newnode=new Node(data);
+if(top==NULL){
+top=newnode;
+}
+else{
+newnode->next=top;
+  top=newnode;
+}
+}
+
+int stack::pop(){
+  if(top==NULL){
+  return -1;
+  }
+  int head=top->data;
+  Node* temp=top;
+  top=top->next;
+  delete temp;
+  return head;
+}
+void stack::display(){
+Node* temp=top;
+while(temp!=NULL){
+std::cout<<temp->data<<" \n";
+  temp=temp->next;
+}
+std::cout<<"\n";
+}
+
+int main(){
+stack s;
+s.push(42);s.push(232);s.push(53);s.push(90);
+std::cout<<"pop: " <<s.pop();
+s.display();
+return 0;
+}
+
+
+
+
+
+
+
+
+
