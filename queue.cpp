4c5fe90722631076f039ebb5fc7218b19ade2f95
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-05-02 10:33:46 +0530

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -46,3 +46,20 @@ void queue::enqueue(int data){
 
 */
 
+
+#include<iostream>
+class Node{
+public:
+int data;
+Node* next;
+Node(){
+  data=0;
+  next=NULL;
+}
+  Node(int data){
+  this->data=data;
+  this->next=NULL;
+  }
+};
+  
+
