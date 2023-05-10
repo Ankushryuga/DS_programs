ab7aa0cad2be85ac100f98eae5f9b324fd946a3e
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-05-10 10:36:44 +0530

    Update circular_queue_implementation_array.cpp

--- circular_queue/circular_queue_implementation_array.cpp
+++ circular_queue/circular_queue_implementation_array.cpp
@@ -66,3 +66,92 @@ circular_queue cq;
 4 5 6 7
 4 5 6 7 8
 */
+
+
+
+
+
+
+
+
+
+
+
+
+/*Circular queue implementation using array */
+
+#include<iostream>
+#define maxsize 5
+
+class circular_queue{
+public:
+int fornt, rear;
+int array[maxsize];
+circular_queue(){
+front=rear=-1;
+}
+  void enqueue(int);
+  int dequeue();
+  void display();
+};
+
+void circular_queue::enqueue(int data){
+if(front==-1 && rear==-1){
+front=rear=0;
+}
+  else if((rear+1)%maxsize==front){
+  std::cout<<"Queue is full:\n";
+    return ;
+  }
+  else
+  rear=(rear+1)%maxsize;
+    array[rear]=data;
+  
+}
+
+
+int circular_queue::dequeue(){
+  int item;
+  if(front==-1 && rear==-1){
+  std::cout<<"Queue is empty:\n";
+    return -1;
+  }
+  else if(front==rear){
+  front=rear=-1;
+  }
+  else{
+    item=array[front];
+  }
+  front=(front+1)%maxsize;
+  return item;
+}
+
+void circular_queue::display(){
+int count=((rear+maxsize-fornt)%maxsize)+1;
+  for(int i=0;i<count;i++){
+  std::cout<<array[(front+i)%maxsize]<<" ";
+  }
+  std::cout<<"/n";
+}
+
+
+int main(){
+circular_queue cq;
+  cq.enqueue(12);cq.enqueue(13);cq.enqueue(14);cq.enqueue(15);cq.enqueue(16);cq.display();
+  std::cout<<"Dequeued: "<<cq.dequeue()<<"\n";
+cq.enqueue(17);cq.display();
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
