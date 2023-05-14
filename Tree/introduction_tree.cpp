bcdbabacab07e87c71caa8048bccde05b8e37664
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-05-14 13:44:26 +0530

    Update introduction_tree.cpp

--- Tree/introduction_tree.cpp
+++ Tree/introduction_tree.cpp
@@ -1,1 +1,20 @@
 
+/* Tree is hierarichal DS 
+Types of tree.
+Binary tree.
+Binary search tree.
+AVL tree 
+B tree
+*/
+
+/* Binary tree either have 2 children or no children.
+Syntax using oops.
+in c++.
+class Node{
+public:
+int data;
+Node* left;
+Node* right;
+};
+
+*/
