a3a0396df6bc80adc8fe6031ce3092a951f68bd2
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-05-16 08:48:30 +0530

    Update BST.cpp

--- Tree/BST.cpp
+++ Tree/BST.cpp
@@ -1,1 +1,29 @@
-/* Binary search tree */
+/* Binary search tree 
+left subtree is either equal to or less than root.
+right subtree is either equal to or greater than roo.
+*/
+
+#include<iostream>
+class Node{
+public:
+int data;
+Node* left;
+Node* right;
+Node(){
+data=0;
+left=NULL;
+right;
+}
+Node(int data){
+this->data=data;
+this->left=NULL;
+this->right=NULL;
+}};
+
+class BST{
+Node* root;
+public:
+BST(){
+root=NULL;
+}
+};
