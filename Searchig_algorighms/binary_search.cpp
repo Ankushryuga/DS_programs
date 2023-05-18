fcc2edcb7fc869e7d06edfa63e19ba03a0999943
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-05-18 09:36:09 +0530

    Update binary_search.cpp

--- Searchig_algorighms/binary_search.cpp
+++ Searchig_algorighms/binary_search.cpp
@@ -16,11 +16,28 @@ int binary_search(std::vector<int>&array, int key){
   }
   return -1;
 }
+int helper(std::vector<int>&array, int key, int s, int e){
+  while(s<e){
+    int mid=(s+e)/2;
+    if(array[mid]==key){
+      return mid;
+    }else if(array[mid]>key){
+      return helper(array, key, s, mid-1);
+    }else if(array[mid]<key){
+      return helper(array, key, mid+1, e);
+    }
+  }
+  return -1;
+}
+
+int binary_search_recursion(std::vector<int>&array, int key){
+    std::cout<<"Last element of array: "<<array[array.size()-1]<<"\n";
+return helper(array, key, 0, array.size());
+}
 
 int main(){
   std::vector<int>array{1,2,3,4,5,6,7,8,9};
-  int k=5;
-  std::cout<<binary_search(array, k)<<"\n";
-  
+  int k=9;
+  std::cout<<binary_search_recursion(array, k)<<"\n";
   return 0;
 }
