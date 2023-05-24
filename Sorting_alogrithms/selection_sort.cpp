beb7844faf991198b3d1ac37e81ecc88fd7b9d1e
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-05-24 08:42:46 +0530

    Update selection_sort.cpp

--- Sorting_alogrithms/selection_sort.cpp
+++ Sorting_alogrithms/selection_sort.cpp
@@ -1,1 +1,13 @@
 /**Implementation of selection sort in c++ */
+
+#include<iostream>
+#include<vector>
+
+void selection_sort(std::vector<int>&array){
+}
+
+int main(){
+std::vector<int>array{2,24,124,121,1,53,64};
+  selection_sort(array);
+  return 0;
+}
