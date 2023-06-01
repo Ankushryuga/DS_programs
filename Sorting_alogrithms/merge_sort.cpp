b0ebd023b2a6d526c3c9b1869b9e473500696b37
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-06-01 09:53:19 +0530

    Update merge_sort.cpp

--- Sorting_alogrithms/merge_sort.cpp
+++ Sorting_alogrithms/merge_sort.cpp
@@ -1,12 +1,14 @@
 /** merge sort implementation using c++ */
 /** Based on divide and conqeror */
 
+
 #include<iostream>
 #include<vector>
 void sort(std::vector<int>&array){}
 
 int main(){
-
+  std::vector<int>array{13,24,154,64,14,90,88}
+  
   
   return 0;
 }
