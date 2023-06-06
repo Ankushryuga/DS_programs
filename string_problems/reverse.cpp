82d0a8799528c32acdfee7672d0c87faa20be806
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-06-06 09:06:16 +0530

    Update reverse.cpp

--- string_problems/reverse.cpp
+++ string_problems/reverse.cpp
@@ -1,2 +1,22 @@
 /* Implementation
 */
+#include<iostream>
+#include<string>
+
+std::string reverse_string(std::string message){
+//std::string temp="";
+  int i=message.length();
+  for(int j=0;j<i/2;j++){
+    char t;
+    t=message[j];
+    message[j]=message[i-j-1];
+    message[i-j-1]=t;
+  }
+  return message;
+}
+
+int main(){
+std::string temp="test";
+  std::cout<<"reverse_string is=> "<<reverse_string(temp)<<" \n";
+  return 0;
+}
