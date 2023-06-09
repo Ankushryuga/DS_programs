c408b49cdf7e43f3ed9e3eecf3438ed27eb9dee4
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-06-09 08:51:29 +0530

    Update longest_palindromic_substring.cpp

--- string_problems/longest_palindromic_substring.cpp
+++ string_problems/longest_palindromic_substring.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
 
+ std::string longestPalindrome(std::string s) {
+        int len=s.size();
+        int tempres[len][len];
+        memset(tempres, 0, sizeof(tempres));
+        int start=0;
+        int end=1;
+        for(int i=0;i<len;i++){
+            tempres[i][i]=1;
+        }
+        for(int i=0;i<len-1;i++){
+            if(s[i]==s[i+1]){
+                tempres[i][i+1]=1;
+                start=i;
+                end=2;
+            }
+        }
+        for(int i=2;i<len;i++){
+            for(int j=0;j<len-i;j++){
+                int left=j;
+                int right=i+j;
+                if(tempres[left+1][right-1]==1 && s[left]==s[right]){
+                    tempres[left][right]=1;
+                    start=j;
+                    end=i+1;
+                }
+            }
+        }
+        return s.substr(start,end);
+        
+    }
