77dc7006e72a6e6d507fd3a42a4fcdae2e0359cc
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-06-12 08:50:48 +0530

    Update heapsort.cpp

--- Sorting_alogrithms/heapsort.cpp
+++ Sorting_alogrithms/heapsort.cpp
@@ -1,8 +1,12 @@
 /** Implementation of heap sort using c++  */
-
-//new approach..
-
 /* Heap sort works on two concepts 1. maxheap or minheap 2. heapify. */
 #include<iostream>
 #include<vector>
 void heapify(std::vector<int>&array){}
+
+void heapsort(){}
+
+int main(){
+
+  return 0;
+}
