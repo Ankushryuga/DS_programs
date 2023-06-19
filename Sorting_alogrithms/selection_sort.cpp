c974fb83e09fd8f6850904e2b8087fddd8744f2c
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-06-19 10:11:50 +0530

    Update selection_sort.cpp

--- Sorting_alogrithms/selection_sort.cpp
+++ Sorting_alogrithms/selection_sort.cpp
@@ -1,13 +1,33 @@
-/**Implementation of selection sort in c++ */
+
+/** Implementation of selection sort
+    Selection sort works as selecting single element from unsorted array and moving it to corresponding position.
+**/
+
 
 #include<iostream>
 #include<vector>
 
-void selection_sort(std::vector<int>&array){
+#include<iostream>
+#include<vector>
+
+void selection_sort(std::vector<int>&arr){
+  int i=0, j=0, min_index;
+  for(int i=0;i<arr.size()-1;i++){
+    min_index=i;
+    for(int j=i+1;j<arr.size();j++){
+      if(arr[j]<arr[min_index]){
+        int temp=arr[min_index];
+        arr[min_index]=arr[j];
+        arr[j]=temp;
+      }
+    }
+  }
 }
 
 int main(){
 std::vector<int>array{2,24,124,121,1,53,64};
   selection_sort(array);
+  for(int i=0;i<array.size();i++)
+  std::cout<<array[i]<<" ";
   return 0;
 }
