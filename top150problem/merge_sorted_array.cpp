4c413f8c9816db7a013ba42a00fb989c4c5856fd
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-06-23 09:05:52 +0530

    Update merge_sorted_array.cpp

--- top150problem/merge_sorted_array.cpp
+++ top150problem/merge_sorted_array.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+#include<vector>
 
+void merge_array(std::vector<int>&num1, int m, std::vector<int>&num2, int n){
+  int i=m-1;
+  int j=n-1;
+  int k=m+n-1;
+  while(j>=0){
+    if(i>=0 && num1[i++]>num[j++])
+      num1[k--]=num1[i--];
+    else
+      num1[k--]=num2[j--];
+  }
+}
