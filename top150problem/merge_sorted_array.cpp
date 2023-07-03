4350e09490393dbcc99ac810c658e9b4c7edbdf6
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-07-03 08:52:39 +0530

    Update merge_sorted_array.cpp

--- top150problem/merge_sorted_array.cpp
+++ top150problem/merge_sorted_array.cpp
@@ -1,6 +1,7 @@
+/* Merge two sorted array into one */
+
 #include<iostream>
 #include<vector>
-
 void merge_array(std::vector<int>&num1, int m, std::vector<int>&num2, int n){
   int i=m-1;
   int j=n-1;
