2c2214a99741079ac6ce7f46776bd7c7b772617a
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-07-05 10:17:14 +0530

    Update 2.remove_element.cpp

--- top150problem/2.remove_element.cpp
+++ top150problem/2.remove_element.cpp
@@ -1,1 +1,10 @@
 
+void remove_element(std::vector<int>&nums, int val){
+  int res=0;
+  for(int i=0;i<nums.size();i++){
+    if(nums[i]!=val){
+      nums[res++]=nums[i+1];
+    }
+  }
+  return nums;
+}
