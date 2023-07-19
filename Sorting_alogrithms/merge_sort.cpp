ce710758cbfc6666f1cbe318ef306168b14d85c4
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-07-19 18:21:01 +0530

    Update merge_sort.cpp

--- Sorting_alogrithms/merge_sort.cpp
+++ Sorting_alogrithms/merge_sort.cpp
@@ -4,8 +4,15 @@
 
 #include<iostream>
 #include<vector>
-void merge_sort(std::vector<int>&array){}
-void merge(std::vector<int>&array, int start, int end){}
+void merge_sort(std::vector<int>&array, int start, int end){
+  // int start=0;
+  // int end=array.size()-1;
+  int mid=(start+end)/2;
+  merge_sort(array, start, mid);
+  merge_sort(array, mid+1, end);
+  merge(array, start, mid, end);
+}
+void merge(std::vector<int>&array, int start, int mid, int end){}
 int main(){
   std::vector<int>array{13,24,154,64,14,90,88}
   
