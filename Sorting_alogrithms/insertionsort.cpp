84be0368c9e8f4caca2b727e2cc76355f9e8198b
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2023-07-20 19:34:43 +0530

    Update insertionsort.cpp

--- Sorting_alogrithms/insertionsort.cpp
+++ Sorting_alogrithms/insertionsort.cpp
@@ -6,7 +6,7 @@ void insertion_sort(std::vector<int>& array){
   int len=array.size();
   for(int i=0;i<len;i++){
     for(int j=i+1;j<len;j++){
-      if(array[i]>array[j]){
+      if(array[i]>array[j]){ 
         int temp=array[i];
         array[i]=array[j];
         array[j]=temp;
